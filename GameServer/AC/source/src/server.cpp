393c44aff1ad4ec78b0502b372e0bb6b79bee00e
CodeHotel <89632518+CodeHotel@users.noreply.github.com>
2025-05-09 02:07:38 +0900

    feature test for immediate team switching fix 5

--- GameServer/AC/source/src/server.cpp
+++ GameServer/AC/source/src/server.cpp
@@ -3296,11 +3296,13 @@ void process(ENetPacket* packet, int sender, int chan)
             //TODO: change team
             loopv(clients) if (clients[i]->type != ST_EMPTY)
             {
+                printf("Checking if client valid\n");
                 if (!valid_client(i)) return;
-
+                printf("Client is valid\n");
                 bool in_team1 = false, in_team2 = false;
                 for (int j = 0; j < MAX_PLAYERS_PER_TEAM; ++j)
                 {
+                    printf("Comparing client name : %s, argteam1 name : %s, argteam2 name : %s",clients[i]->name,scl.argteam1[j],scl.argteam2[j]);
                     if (!strcmp(clients[i]->name, scl.argteam1[j])) { in_team1 = true; break; }
                     if (!strcmp(clients[i]->name, scl.argteam2[j])) { in_team2 = true; break; }
                 }
