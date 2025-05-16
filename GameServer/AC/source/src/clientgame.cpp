e7ddf33525cbc6bfd87b324892e5c5640014b7c6
la2yness <145730780+la2yness@users.noreply.github.com>
2025-05-17 00:17:17 +0900

    Fixed aimbot123

--- GameServer/AC/source/src/clientgame.cpp
+++ GameServer/AC/source/src/clientgame.cpp
@@ -661,8 +661,7 @@ void resetsleep(bool force)
 COMMANDF(resetsleeps, "", (void) { resetsleep(true); });
 
 // 에임핵 기능 구현
-int aimBotType = 1; // 1: 끔, 2: 다이렉트(1), 3: Lerp(20), 4: Ease-out + 0.15초 딜레이 + 락온 시스템
-int espFlag = 0;    // 0: 끔, 1: 켬
+
 
 void init_hack_settings()
 {
--- GameServer/AC/source/src/pch.cpp
+++ GameServer/AC/source/src/pch.cpp
@@ -1,1 +1,4 @@
 #include "cube.h"
+
+int aimBotType = 1; // 1: 끔, 2: 다이렉트(1), 3: Lerp(20), 4: Ease-out + 0.15초 딜레이 + 락온 시스템
+int espFlag = 0;    // 0: 끔, 1: 켬
